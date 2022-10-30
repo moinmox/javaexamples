ba10782c37da4dc4ccdd341f88ed6a891dcba53e
Moin Pasha <116783467+moinmox@users.noreply.github.com>
2022-10-30 22:17:57 +0530

    Add files via upload

---
+++ oddoreven.java
@@ -0,0 +1,17 @@
+package javaexamples;
+import java.util.Scanner;
+public class oddoreven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		num=sc.nextInt();
+		if(num%2==0)
+			System.out.println("number is even");
+		else
+			System.out.println("number is odd");
+	}
+
+}
