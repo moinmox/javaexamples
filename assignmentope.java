ff7d68d72ab0381dd97e8c39cb1878549a82bb02
Moin Pasha <116783467+moinmox@users.noreply.github.com>
2022-10-30 22:43:34 +0530

    Add files via upload

---
+++ assignmentope.java
@@ -0,0 +1,20 @@
+package javaexamples;
+
+public class assignmentope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		num1+=5;//action of num1=num1+5
+		System.out.println("The output of num1+=5 is"+num1);
+		num1-=5;//action of num1=num1-5
+		System.out.println("The output of num1-=5 is"+num1);
+		num1*=5;//action of num1=num1*5
+		System.out.println("The output of num1*=5 is"+num1);
+		num1/=5;//action of num1=num1/5
+		System.out.println("The output of num1/=5 is"+num1);
+		num1%=5;//action of num1=num1%5
+		System.out.println("The output of num1%=5 is"+num1);
+	}
+
+}
