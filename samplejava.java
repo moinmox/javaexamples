762d75651f70a07606d7bf56899f7d862955dd25
Moin Pasha <116783467+moinmox@users.noreply.github.com>
2022-10-30 22:39:36 +0530

    Add files via upload

---
+++ samplejava.java
@@ -0,0 +1,15 @@
+package javaexamples;
+
+public class samplejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=6,c=7;
+		b=a++;
+		c=++a;
+		System.out.println("a:"+a+"b:"+b);
+		System.out.println("a:"+a+"c:"+c);
+
+	}
+
+}
